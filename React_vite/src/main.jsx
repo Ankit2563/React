d861dbd350f8cfaa52a53b20905627c4af01c44b
ANKIT <ankitcloud2563@gmail.com>
2024-01-06 01:45:46 +0530

    different different type of HTML form ultimately react parse it to convert to a specific type

--- React_vite/src/main.jsx
+++ React_vite/src/main.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    // parsing happen  to change in other form 
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "click me to visit google",
+};
 
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  
+  
+  // MyApp()
+  // <MyApp/>
+  // reactElement
+  anotherElement
+
+    
+ 
 )
