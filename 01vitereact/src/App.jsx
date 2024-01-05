d44a5bc2fd738cafa7036fbe78a41bddce149332
ANKIT <ankitcloud2563@gmail.com>
2024-01-05 21:02:36 +0530

    React

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Chai from "./chai.jsx"
+
+function App() {
+  
+
+  return (
+    //  <h1>This is our project</h1>
+    //this tag is called fragment
+    <>
+      <Chai />
+      <p>this is adding component</p>
+    </>
+  );
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,11 @@
+// this function is component it must be capital first letter conventially
+// either by vite or create react
+//good practice is to capitalise the file first name
+// for vite jsx file is must not js
+
+function Chai() {
+    return (
+        <h3>chai is ready</h3>
+    )
+}
+export default Chai;
